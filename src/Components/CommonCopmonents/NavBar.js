5082ce9c7b32fd5e530c1f2417be06a8c4e641d7
jonathan westfall <jonathan.westfall1@gmail.com>
2021-01-05 15:47:01 -0600

    Added the navbar
    Made a js file for the navbar which can be put at the top or bottom.

---
+++ src/Components/CommonCopmonents/NavBar.js
@@ -0,0 +1,39 @@
+import React, {useState, useEffect} from 'react';
+import '../../Bootstrap/css/bootstrap.css';
+
+function NavBar() {
+    return (
+      <div >
+          <nav className="navbar navbar-expand bg-success navbar-dark flex-row-reverse" >
+                <div className="collaspe navbar-collaspe">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#"><span>Home</span> </a>
+                            </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#"><span>Menu</span> </a>
+                            </li>
+                        <li className="nav-item">
+                                <a className="nav-link" href="#"><span>Locations</span> </a>
+                            </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#"><span>About Us</span> </a>
+                            </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#"><span>Contact</span> </a>
+                            </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#"><span>Order Online</span> </a>
+                            </li>
+                    </ul>
+
+                </div>
+            </nav>
+
+      </div>
+    );
+  }
+
+  export default NavBar;
+
+  
