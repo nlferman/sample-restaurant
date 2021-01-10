62ade4568a654fc7b470bd68031eeb4940c78fed
goeteeks <goeteeks@gmail.com>
2021-01-10 18:44:12 -0500

    Added Bootstrap for react

--- src/Components/CommonCopmonents/CarouselHeader.js
+++ src/Components/CommonCopmonents/CarouselHeader.js
@@ -1,46 +1,54 @@
-import '../../Bootstrap/css/bootstrap.css';
+import {useState} from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 import {pancakes} from '../../Images/Images';
 
 const CarouselHeader = () => {
+    const [index, setIndex] = useState(0);
+    
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    };
+    
     return (
-        <div id="carouselExampleIndicators bg-danger" className="carousel slide" data-bs-ride="carousel">
-            <ol className="carousel-indicators">
-                <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active"></li>
-                <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
-                <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
-            </ol>
-            <div className="carousel-inner">
-                <div className="carousel-item active">
-                <img src={pancakes} className="d-block w-100" alt="Breakfast" />
-                <div className="carousel-caption d-none d-md-block">
-                    <h5>Breakfast</h5>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </div>
-                </div>
-                <div className="carousel-item">
-                <img src={pancakes} className="d-block w-100" alt="Lunch" />
-                <div className="carousel-caption d-none d-md-block">
-                    <h5>Lunch</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-                </div>
-                <div className="carousel-item">
-                <img src={pancakes} className="d-block w-100" alt="Dinner" />
-                <div className="carousel-caption d-none d-md-block">
-                    <h5>Dinner</h5>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </div>
-                </div>
-            </div>
-            <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
-            </a>
-            <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Next</span>
-            </a>
-      </div>
+        <Carousel activeIndex={index} onSelect={handleSelect}>
+        <Carousel.Item>
+            <img
+            className="d-block w-100"
+            src="holder.js/800x400?text=First slide&bg=373940"
+            alt="First slide"
+            />
+            <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+            <img
+            className="d-block w-100"
+            src="holder.js/800x400?text=Second slide&bg=282c34"
+            alt="Second slide"
+            />
+    
+            <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+            <img
+            className="d-block w-100"
+            src="holder.js/800x400?text=Third slide&bg=20232a"
+            alt="Third slide"
+            />
+    
+            <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+            </Carousel.Caption>
+        </Carousel.Item>
+        </Carousel>
     );
   }
 
