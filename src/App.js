53e9fbc0bbfee9045ed02f4e3478ce6046bb3a1c
jonathan westfall <jonathan.westfall1@gmail.com>
2021-01-10 18:49:19 -0600

    Added the test card to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/CommonCopmonents/NavBar';
+import Card from './Components/CommonCopmonents/Card';
 function App() {
   return (
     <div className="App">
     <NavBar/>
+    <Card myprops= {"test"} showDescription= {""}/>
     </div>
   );
 }
