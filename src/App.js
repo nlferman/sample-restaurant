043f3697e97dd884e41a2c6890d21318b43c4f2b
jonathan westfall <jonathan.westfall1@gmail.com>
2021-01-10 19:09:14 -0600

    Added the carousel in app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/CommonCopmonents/NavBar';
+<<<<<<< HEAD
 import Card from './Components/CommonCopmonents/Card';
 function App() {
   return (
     <div className="App">
     <NavBar/>
     <Card myprops= {"test"} showDescription= {""}/>
+=======
+import CarouselHeader from './Components/CommonCopmonents/CarouselHeader';
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <NavBar/>
+      </div>
+      <div>
+        <CarouselHeader />
+      </div>
+>>>>>>> 62ade4568a654fc7b470bd68031eeb4940c78fed
     </div>
   );
 }
