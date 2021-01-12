53a651f6bef0b3e02ad4c5504588ee60858b77c7
goeteeks <goeteeks@gmail.com>
2021-01-11 21:14:56 -0500

    Templated Carousel.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import {useEffect, useState} from 'react';
+import {MainPage} from './Template/MainPage.js';
 import './App.css';
 import NavBar from './Components/CommonCopmonents/NavBar';
 import Card from './Components/CommonCopmonents/Card';
 import CarouselHeader from './Components/CommonCopmonents/CarouselHeader';
+
 function App() {
+  const [mainPage, setMainPage] = useState();
+
+  useEffect(() => {
+    console.log("Something!");
+    console.log("Main Page:", MainPage);
+    const mainPageData = MainPage;
+    setMainPage(mainPageData);
+  }, []);
+
   return (
     <div className="App">
-      <NavBar/>
-      <Card myprops= {"test"} showDescription= {""}/>
-      <CarouselHeader />
+      {mainPage === undefined ? null : 
+        <div alignItems={'center'}>
+        <NavBar/>
+        <Card myprops= {"test"} showDescription= {""}/>
+        <CarouselHeader carouselData={mainPage} />
+        </div>
+      }
     </div>
   );
 }
--- src/Components/CommonCopmonents/CarouselHeader.js
+++ src/Components/CommonCopmonents/CarouselHeader.js
@@ -1,52 +1,62 @@
 import {useState} from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import {pancakes} from '../../Images/Images';
+// import breakfast from '../../../public/Images/panckaes.png';
+// import lunch from '../../../public/Images/club_sandwich.png';
+// import dinner from '../../Images/steak.png';
 
-const CarouselHeader = () => {
+const CarouselHeader = (props) => {
     const [index, setIndex] = useState(0);
     
     const handleSelect = (selectedIndex, e) => {
         setIndex(selectedIndex);
     };
+
+    console.log(props);
+    const {carouselOptions} = props.carouselData;
+
+    const makeCarousel = carouselItems => {
+        const carousel = [];
+        carouselItems.forEach((carouselItem) => {
+            carousel.push
+            (
+                <Carousel.Item className={carouselItem.classNameItem} width={"400px"} interval={carouselItem.interval}>
+                    <img
+                    className="d-block w-100"
+                    src={carouselItem.image.default}
+                    alt={carouselItem.altText}
+                    width={"40px"}
+                    height={"300px"}
+                    style={{objectFit:'contain'}}
+                    />
+                    <Carousel.Caption className={carouselItem.classNameCaption}>
+                    <h3>{carouselItem.title}</h3>
+                    <p>{carouselItem.description}</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            )
+        });
+        return carousel;
+    }
     
     return (
-        <Carousel activeIndex={index} onSelect={handleSelect} >
-            <Carousel.Item className="container-fluid" >
-                <img
-                className="d-block w-100"
-                src={pancakes}
-                alt="Breakfast"
-                
-                />
-                <Carousel.Caption>
-                <h3>Breakfast</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item className="container-fluid" >
-                <img
-                className="d-block w-100"
-                src={'../../Images/club_sandwich.png'}
-                alt="Lunch"
-                
-                />
-                <Carousel.Caption>
-                <h3>Lunch</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item className="container-fluid" >
-                <img
-                className="d-block w-100"
-                src={'../../Images/steak.png'}
-                alt="Dinner"
-                
-                />
-                <Carousel.Caption>
-                <h3>Dinner</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
+        <Carousel
+            className={carouselOptions.className}
+            activeIndex={index}
+            onSelect={handleSelect}
+            style={{width: 400}}
+            controls={carouselOptions.showControls}
+            indicators={carouselOptions.showIndicators}
+            interval={carouselOptions.defaultInterval === null ? 5000 : carouselOptions.defaultInterval}
+            fade={carouselOptions.fade}
+            keyboard={carouselOptions.keybgoard}
+            pause={carouselOptions.pause === 'hover' ? 'hover' : false}
+            nextLabel={carouselOptions.nextLabel === '' || carouselOptions.nextLabel === undefined ? 'Next' : carouselOptions.nextLabel}
+            prevLabel={carouselOptions.prevLabel === '' || carouselOptions.prevLabel === undefined ? 'Previous' : carouselOptions.prevLabel}
+            slide={carouselOptions.slide}
+            touch={carouselOptions.touch}
+            wrap={carouselOptions.wrap}
+             >
+            {makeCarousel(props.carouselData.carouselItems)}
         </Carousel>
     );
   }
